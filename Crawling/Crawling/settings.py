744f1fd4d9a1526c575ed30c19e64bc349ca6e9e
iamsoojung <crystar2402@naver.com>
2024-09-09 17:59:58 +0900

    chore: crawling secret key 설정

--- Crawling/Crawling/settings.py
+++ Crawling/Crawling/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +122,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#######################################################
+
+#environ을 기존 import에 추가
+import environ
+
+env = environ.Env(DEBUG=(bool, True)) #환경변수를 불러올 수 있는 상태로 세팅
+
+#환경변수 파일 읽어오기
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+
+GEN_AI_SECRET_KEY = env('GEN_AI_SECRET_KEY')
+CRAWLING_USER_AGENT = env('CRAWLING_USER_AGENT')

5730165e79200d8e565f09702b0820d605a14e22
이예찬 <dldpcks34@naver.com>
2024-09-10 14:46:14 +0900

    feat: 회원 모델 추가
    서비스 사용을 위한 회원 엔티티 개발
    
    Related to:

---
+++ backend/src/main/java/com/newlearn/backend/user/model/Users.java
@@ -0,0 +1,82 @@
+package com.newlearn.backend.user.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(of = "userId")
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long userId;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "nickname", nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "difficulty")
+	private Long difficulty;
+
+	@Column(name = "total_news_read_count")
+	private Long totalNewsReadCount;
+
+	@Column(name = "total_word_count")
+	private Long totalWordCount;
+
+	@Column(name = "scrap_count")
+	private Long scrapCount;
+
+	@Column(name = "experience")
+	private Long experience;
+
+	@Setter
+	@ManyToMany
+	@JoinTable(
+		name = "user_interest",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "interest_id")
+	)
+	private Set<Category> interests = new HashSet<>();
+
+	@Column(name = "created_at", nullable = false, updatable = false,
+		insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+
+
+
+
+
+}

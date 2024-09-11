d02c05a7a798abc900d36144c2343505b9a62551
손민락 <dungguk@naver.com>
2024-09-11 17:22:32 +0900

    design: 404 Page 디자인

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -5,10 +6,26 @@ const Container = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  text-align: center;
+  div {
+    margin-bottom: 1.5rem;
+    font-size: 3rem;
+    font-weight: 700;
+  }
+  a {
+    text-decoration: none;
+    font-size: 1.5rem;
+    color: ${(props) => props.theme.colors.text02};
+  }
 `;
 
 const NotFoundPage = () => {
-  return <Container>404 Error</Container>;
+  return (
+    <Container>
+      <div>Page Not Found!</div>
+      <Link to="/">메인으로 돌아가기</Link>
+    </Container>
+  );
 };
 
 export default NotFoundPage;

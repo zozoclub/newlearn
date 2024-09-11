1084ee48f78b08feeb224bd4bca662514543ba7f
손민락 <dungguk@naver.com>
2024-09-11 12:48:33 +0900

    fix: 배경 애니메이션 자연스럽게 되도록 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "@context/ThemeContext";
 import { GlobalStyle } from "@styles/GlobalStyle";
+import MainPage from "@pages/MainPage";
+import styled from "styled-components";
+import Background from "@styles/BackGround";
+
+const AppContainer = styled.div`
+  position: relative;
+  background-color: none;
+`;
 
 const App: React.FC = () => {
   return (
     <RecoilRoot>
       <ThemeProvider>
         <GlobalStyle />
-        <MainPage />
+        <AppContainer>
+          <Background />
+          <MainPage />
+        </AppContainer>
       </ThemeProvider>
     </RecoilRoot>
   );
---
+++ frontend/src/styles/BackGround.tsx
@@ -0,0 +1,40 @@
+import { useTheme } from "@context/ThemeContext";
+import { Theme } from "types/theme";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  z-index: -1;
+`;
+const LightBackground = styled.img.attrs({
+  src: "src/assets/images/background-light.png",
+  alt: "",
+})`
+  position: fixed;
+  z-index: 1;
+  width: 1925px;
+`;
+
+const DarkBackground = styled.img.attrs({
+  src: "src/assets/images/background-dark.png",
+  alt: "",
+})<{ theme: Theme }>`
+  position: fixed;
+  z-index: 2;
+  opacity: ${(props) => props.theme.opacities.background};
+  transition: opacity 0.5s;
+  width: 1925px;
+`;
+
+const Background = () => {
+  const { theme } = useTheme();
+
+  return (
+    <Container>
+      <LightBackground theme={theme} />
+      <DarkBackground theme={theme} />
+    </Container>
+  );
+};
+
+export default Background;

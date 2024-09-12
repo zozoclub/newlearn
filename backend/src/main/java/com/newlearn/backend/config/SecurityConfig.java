51ee8b1def9c78df004b3d8d468a90604a1d8434
이예찬 <dldpcks34@naver.com>
2024-09-12 13:09:53 +0900

    feat: OAuth2 인증 흐름 및 사용자 정보 요청 및 처리 로직 구현 (#S11P21D105-330)
    프로젝트에서 인증, 권한 처리할 스프링 시큐리티 설정 및 Oauth2 접근 허용

---
+++ backend/src/main/java/com/newlearn/backend/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.newlearn.backend.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import com.newlearn.backend.common.JwtTokenProvider;
+import com.newlearn.backend.oauth.service.CustomOAuth2UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+	@Value("${cors.allowed-origin}")
+	private String[] allwoedOrigins;
+
+	@Value("${cors.allowed-methods}")
+	private String[] allowedMethods;
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.cors(cors -> cors.configurationSource(request -> {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Arrays.asList(allwoedOrigins));
+				config.setAllowedMethods(Arrays.asList(allowedMethods));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+				config.setMaxAge(3600L);
+				return config;
+			}))
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(requests -> requests
+				.requestMatchers("/api/oauth/**", "/api/user/sign-up").permitAll()
+				.anyRequest().authenticated())
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+				.successHandler(oAuth2AuthenticationSuccessHandler)
+			)
+			.exceptionHandling(exception -> exception
+				.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+				.accessDeniedHandler(new CustomAccessDeniedHandler())
+			)
+			.formLogin(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}

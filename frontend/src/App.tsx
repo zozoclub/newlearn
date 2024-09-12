122356b75d99447078d792be024f72452951c682
손민락 <dungguk@naver.com>
2024-09-12 17:50:09 +0900

    feat: 라우트 이동 시 fadeout, fadein 효과를 적용(중간 저장)
    수정해야 할 방향 :
    fadeOut 이 끝나면 페이지 이동을 하고 fadeIn을 해야 하는데
    현재는 페이지 이동 후 fadeOut, fadeIn이 이루어지고 있음.
    이것은 Navbar에서 navigate를 하고 있고
    애니메이션은 App에서 적용하고 있는 것으로 추정됨.
    
    그래서 Navbar에 현재 App에 있는 location과 setDisplayLocation 등을 설정하고
    500ms가 지난 후에 navigate를 함으로써 fadeOut을 하는 동안에는 원래 있던 페이지에 머물러 있도록 해야함.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom";
-import styled from "styled-components";
+import { Outlet, useLocation } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
 
 import Header from "@components/Header";
 import Navbar from "@components/Navbar";
+import { useEffect, useState } from "react";
 
 const AppContainer = styled.div`
   position: relative;
@@ -10,20 +11,65 @@ const AppContainer = styled.div`
   padding: 0 5vw;
 `;
 
-const Content = styled.div`
-  height: calc(100% - 150px);
-`;
+// const Content = styled.div`
+//   height: calc(100% - 150px);
+// `;
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransitionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransitionStage("fadeOut");
+  }, [location]);
+
   return (
     <AppContainer>
       <Header />
-      <Content>
+      <Content
+        className={`${transitionStage}`}
+        onAnimationEnd={() => {
+          setTransitionStage("fadeIn");
+          setDisplayLocation(location);
+        }}
+      >
         <Outlet />
       </Content>
       <Navbar />
     </AppContainer>
   );
 };
+
+export const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+export const fadeOutAnimation = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+export const Content = styled.div`
+  height: calc(100% - 150px);
+  opacity: 0;
+  &.fadeIn {
+    animation: ${fadeInAnimation} 500ms;
+    animation-fill-mode: forwards;
+  }
+  &.fadeOut {
+    animation: ${fadeOutAnimation} 500ms;
+    animation-fill-mode: forwards;
+    // animation-fill-mode :애니메이션의 끝난 후의 상태를 설정
+    // forward : 애니메이션이 끝난 후 마지막 CSS 그대로 있음
+  }
+`;
 
 export default App;

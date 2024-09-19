a48b17702d073224ac3813f6e3816e9ffada9a62
박효진 <jam_ty@naver.com>
2024-09-19 12:13:05 +0900

    style : 누락된 '#' 추가

--- frontend/src/styles/theme/darkTheme.ts
+++ frontend/src/styles/theme/darkTheme.ts
@@ -5,7 +5,7 @@ export const darkTheme: Theme = {
     cardBackground: "#333344",
     primary: "#0099ff",
     primaryPress: "#62b7ff",
-    danger: "e9404f",
+    danger: "#e9404f",
     dangerPress: "#ff6573",
     cancel: "#1a1925",
     cancelPress: "#282638",
@@ -15,7 +15,7 @@ export const darkTheme: Theme = {
     text03: "#c1c1c1",
     text04: "#a5a5a5",
     readonly: "#333333",
-    placeholder: "8b8b8b",
+    placeholder: "#8b8b8b",
     shadow: "#0e1125",
   },
   opacities: {

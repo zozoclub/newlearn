9bf3ba2d722df3cf95ce821025c2b7ca3628ece0
이예찬 <dldpcks34@naver.com>
2024-09-22 02:47:10 +0900

    feat: redis 전체 제거 코드 추가 (#S11P21D105-295)
    redis에 저장된 모든 키 제거하는 파이썬 파일 추가

---
+++ Crawling/crawled_data/management/commands/delete_redis.py
@@ -0,0 +1,26 @@
+import redis
+from Crawling import settings
+from django.core.management.base import BaseCommand
+
+try:
+    redis_client = redis.StrictRedis(
+        host='54.180.158.165',
+        port=6379,
+        db=1,
+        username=settings.REDIS_USERNAME,
+        password=settings.REDIS_PASSWORD
+    )
+
+    response = redis_client.ping()
+    print(f"Redis 연결 성공: {response}")
+except redis.ConnectionError as e:
+    print(f"Redis 연결 실패: {e}")
+
+
+
+class Command(BaseCommand):
+    help = '몽고 db랑 redis에 중복되어있는지 확인하는 코드'
+
+    def handle(self, *args, **options):
+        redis_client.flushdb()
+        print("모든 키가 성공적으로 삭제되었습니다.")

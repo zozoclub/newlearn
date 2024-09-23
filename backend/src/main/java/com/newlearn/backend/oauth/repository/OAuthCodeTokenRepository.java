559cf48bf50df4777e1a6742a7402286f12184e7
이예찬 <dldpcks34@naver.com>
2024-09-23 16:24:22 +0900

    Revert "Merge branch 'backend-develop' into 'main'"
    This reverts merge request !33

--- backend/src/main/java/com/newlearn/backend/oauth/repository/OAuthCodeTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.newlearn.backend.oauth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.newlearn.backend.oauth.model.OAuthCodeToken;
-
-public interface OAuthCodeTokenRepository extends CrudRepository<OAuthCodeToken, String> {
-}
--- backend/src/main/java/com/newlearn/backend/oauth/repository/TempUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.newlearn.backend.oauth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.newlearn.backend.oauth.model.TempUser;
-
-public interface TempUserRepository extends CrudRepository<TempUser, String> {
-}

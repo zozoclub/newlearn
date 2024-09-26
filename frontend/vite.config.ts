752e8617a4eb24ba4ceb30c8142960fc5d02a75c
손민락 <dungguk@naver.com>
2024-09-26 13:06:55 +0900

    hotfix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,50 +2,50 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // PWA
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 // import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   plugins: [
     react(),
     // mkcert(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "NewLearn",
-        short_name: "NewLearn",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/pwa-64x64.png", // 절대 경로로 수정
-            sizes: "64x64",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-192x192.png", // 절대 경로로 수정
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png", // 절대 경로로 수정
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/maskable-icon-512x512.png", // 절대 경로로 수정
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-        type: "module",
-      },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+    //   manifest: {
+    //     name: "NewLearn",
+    //     short_name: "NewLearn",
+    //     theme_color: "#ffffff",
+    //     icons: [
+    //       {
+    //         src: "/pwa-64x64.png", // 절대 경로로 수정
+    //         sizes: "64x64",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/pwa-192x192.png", // 절대 경로로 수정
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/pwa-512x512.png", // 절대 경로로 수정
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "/maskable-icon-512x512.png", // 절대 경로로 수정
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //     ],
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //     type: "module",
+    //   },
+    // }),
   ],
   resolve: {
     alias: {

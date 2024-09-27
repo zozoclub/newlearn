c0fc9a6025c5386ba264f689d1b4371ae79857bd
노나현 <selene0106@naver.com>
2024-09-27 16:32:24 +0900

    fix: 뉴스 상세보기 응답 - 단어부분 수정(#S11P21D105-385)

--- backend/src/main/java/com/newlearn/backend/word/repository/WordSentenceRepository.java
+++ backend/src/main/java/com/newlearn/backend/word/repository/WordSentenceRepository.java
@@ -10,26 +10,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface WordSentenceRepository extends JpaRepository<WordSentence, Long> {
-//    @Query("SELECT ws FROM WordSentence ws " +
-//            "LEFT JOIN ws.word w " +
-//            "WHERE ws.newsId = :newsId " +
-//            "AND w.wordId = :wordId " +
-//            "AND ws.difficulty = :difficulty")
-//    Optional<WordSentence> findOptionalByNewsIdAndWordIdAndDifficulty(
-//            @Param("newsId") Long newsId,
-//            @Param("wordId") Long wordId,
-//            @Param("difficulty") Integer difficulty
-//    );
-    @Query("SELECT ws FROM WordSentence ws " +
-            "LEFT JOIN ws.word w " +
+    @Query("SELECT ws " +
+            "FROM WordSentence ws " +
             "WHERE ws.newsId = :newsId " +
-            "AND w.wordId IN :wordIds " +
+            "AND ws.word.wordId IN :wordIds " +
             "AND ws.difficulty = :difficulty")
     List<WordSentence> findByNewsIdAndWordIdsAndDifficulty(
             @Param("newsId") Long newsId,
             @Param("wordIds") List<Long> wordIds,
             @Param("difficulty") Integer difficulty
     );
-
-
 }

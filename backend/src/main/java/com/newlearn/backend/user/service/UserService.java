c7e595279f24e616bf3dae637ed00d3a95ed5588
이예찬 <dldpcks34@naver.com>
2024-09-27 10:11:22 +0900

    feat: 사용자 순위 조회 로직 구현 (#S11P21D105-343)
    - 예외 추가

--- backend/src/main/java/com/newlearn/backend/user/service/UserService.java
+++ backend/src/main/java/com/newlearn/backend/user/service/UserService.java
@@ -33,7 +33,7 @@ public interface UserService {
 
 	void deleteUser(Long userId);
 
-	UserProfileResponseDTO getProfile(Long userId);
+	UserProfileResponseDTO getProfile(Long userId) throws Exception;
 
 	// 마이페이지
 	Page<UserScrapedNewsResponseDTO> getScrapedNewsList(Long userId, NewsPagenationRequestDTO newsPagenationRequestDTO, int difficulty);

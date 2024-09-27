7b65bf5da250769d35acbfa926101d8eaddacfd5
이예찬 <dldpcks34@naver.com>
2024-09-27 09:38:28 +0900

    feat: 사용자 순위 조회 로직 구현 (#S11P21D105-343)
    - conflict 해결하기 위한 코드 수정

--- backend/src/main/java/com/newlearn/backend/user/service/UserService.java
+++ backend/src/main/java/com/newlearn/backend/user/service/UserService.java
@@ -3,15 +3,9 @@ package com.newlearn.backend.user.service;
 import java.util.List;
 import java.util.Optional;
 
-import com.newlearn.backend.news.dto.request.AllNewsRequestDTO;
-import com.newlearn.backend.news.dto.response.NewsResponseDTO;
-import com.newlearn.backend.user.dto.request.NewsPagenationRequestDTO;
 import com.newlearn.backend.user.dto.request.UpdateAvatarDTO;
 import com.newlearn.backend.user.dto.request.SignUpRequestDTO;
-import com.newlearn.backend.user.dto.response.UserProfileResponseDTO;
-import com.newlearn.backend.user.dto.response.UserScrapedNewsResponseDTO;
 import com.newlearn.backend.user.model.Users;
-import org.springframework.data.domain.Page;
 
 public interface UserService {
 
@@ -30,11 +24,5 @@ public interface UserService {
 	void updateCategory(Long userId, List<String> categories);
 
 	void deleteUser(Long userId);
-
-	UserProfileResponseDTO getProfile(Long userId) throws Exception;
-
-	// 마이페이지
-	Page<UserScrapedNewsResponseDTO> getScrapedNewsList(Long userId, NewsPagenationRequestDTO newsPagenationRequestDTO, int difficulty);
-
-	Long getUserRank(Long userId) throws Exception;
 }
+
--- backend/src/main/java/com/newlearn/backend/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/newlearn/backend/user/service/UserServiceImpl.java
@@ -1,35 +1,25 @@
 package com.newlearn.backend.user.service;
 
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
-import com.newlearn.backend.news.model.UserNewsRead;
-import com.newlearn.backend.news.model.UserNewsScrap;
-import com.newlearn.backend.news.repository.UserNewsReadRepository;
-import com.newlearn.backend.news.repository.UserNewsScrapRepository;
-import com.newlearn.backend.user.dto.request.NewsPagenationRequestDTO;
-import com.newlearn.backend.user.dto.response.UserScrapedNewsResponseDTO;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.newlearn.backend.user.dto.request.UpdateAvatarDTO;
 import com.newlearn.backend.user.dto.request.SignUpRequestDTO;
-import com.newlearn.backend.user.dto.response.UserProfileResponseDTO;
 import com.newlearn.backend.user.model.Avatar;
 import com.newlearn.backend.user.model.Category;
 import com.newlearn.backend.user.model.Users;
 import com.newlearn.backend.user.repository.AvatarRepository;
 import com.newlearn.backend.user.repository.CategoryRepository;
 import com.newlearn.backend.user.repository.UserRepository;
-import com.newlearn.backend.word.repository.WordRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import static com.newlearn.backend.user.dto.response.UserScrapedNewsResponseDTO.makeScrapedNewsResponseDTO;
 
 @RequiredArgsConstructor
 @Service
@@ -39,9 +29,6 @@ public class UserServiceImpl implements UserService{
 	private final UserRepository userRepository;
 	private final AvatarRepository avatarRepository;
 	private final CategoryRepository categoryRepository;
-	private final WordRepository wordRepository;
-	private final UserNewsScrapRepository userNewsScrapRepository;
-	private final UserNewsReadRepository userNewsReadRepository;
 
 	@Override
 	public Users findByEmail(String email) throws Exception {
@@ -52,13 +39,6 @@ public class UserServiceImpl implements UserService{
 	@Override
 	@Transactional
 	public void signUp(SignUpRequestDTO signUpRequestDTO) {
-
-		Optional<Users> existingUser = userRepository.findByEmail(signUpRequestDTO.getEmail());
-
-		if (existingUser.isPresent()) {
-			throw new IllegalStateException("이미 사용자가 존재합니다.");
-		}
-
 		Users user = signUpRequestDTO.toUserEntity();
 
 		Set<Category> categories = new HashSet<>();
@@ -135,49 +115,4 @@ public class UserServiceImpl implements UserService{
 		userRepository.deleteById(userId);
 	}
 
-	@Override
-	public UserProfileResponseDTO getProfile(Long userId) throws Exception {
-
-		Users user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다"));
-
-		Long unCount = wordRepository.countCompleteWordsByUser(user);
-		Long Count = wordRepository.countIncompleteWordsByUser(user);
-		Long userRank = getUserRank(userId);
-
-		return new UserProfileResponseDTO(user, unCount, Count, userRank);
-
-	}
-
-	/* 마이페이지 */
-
-	@Override
-	public Page<UserScrapedNewsResponseDTO> getScrapedNewsList(Long userId, NewsPagenationRequestDTO newsPagenationRequestDTO, int difficulty) {
-		Users user = userRepository.findById(userId)
-				.orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
-
-		// 1. UserNewsScrapRepository에서 사용자가 스크랩한 뉴스 가져오기
-		// 전체조회 (유저) : 난이도 별 조회 (유저 & difficulty)
-		Page<UserNewsScrap> newsList = (difficulty == 0)
-				? userNewsScrapRepository.findAllByUserOrderByScrapedDate(user, newsPagenationRequestDTO.getPageable())
-				: userNewsScrapRepository.findAllByUserAndDifficultyOrderByScrapedDate(user, difficulty, newsPagenationRequestDTO.getPageable());
-
-		// 2. 관련된 모든 UserNewsRead를 한 번에 조회
-		// 유저가 스크랩한 뉴스들의 Id 리스트
-		List<Long> newsIds = newsList.getContent().stream()
-				.map(scrap -> scrap.getNews().getNewsId())
-				.collect(Collectors.toList());
-
-		Map<Long, UserNewsRead> readStatusMap = userNewsReadRepository.findAllByUserAndNewsNewsIdIn(user, newsIds).stream()
-				.collect(Collectors.toMap(read -> read.getNews().getNewsId(), Function.identity()));
-
-		// 3. UserScrapedNewsResponseDTO로 변환 및 새로운 Page 객체 생성
-		return newsList.map(scrap -> makeScrapedNewsResponseDTO(scrap, readStatusMap.get(scrap.getNews().getNewsId()), "en", difficulty));
-
-	}
-
-	@Override
-	public Long getUserRank(Long userId) throws Exception {
-		int rank = userRepository.findUserRankById(userId);
-		return Long.valueOf(rank);
-	}
 }

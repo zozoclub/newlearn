9aea3fda295b4238873d6c8b3de353e56dc757b9
허세령 <seryoii@naver.com>
2024-09-30 15:17:25 +0900

    feat: 레벨 아이콘 추가

---
+++ frontend/src/components/common/LevelIcon.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import levelIconsImage from "@assets/images/levelIcon.png";
+
+interface LevelIconProps {
+  level: number;
+  size?: number;
+}
+
+const LevelIcon: React.FC<LevelIconProps> = ({ level, size = 48 }) => {
+  const adjustedLevel = Math.min(Math.max(level - 1, 0), 99);
+  const row = Math.floor(adjustedLevel / 10);
+  const col = adjustedLevel % 10;
+
+  console.log(row, col);
+
+  return (
+    <IconWrapper size={size}>
+      <IconImage row={row} col={col} />
+    </IconWrapper>
+  );
+};
+const IconWrapper = styled.div<{ size: number }>`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  overflow: hidden;
+`;
+
+const IconImage = styled.div<{ row: number; col: number }>`
+  width: 100%;
+  height: 100%;
+  background-image: url(${levelIconsImage});
+  background-size: 1000% 1000%;
+  background-position: ${(props) =>
+    `${props.col * 11.11}% ${props.row * 11.11}%`};
+  image-rendering: pixelated;
+`;
+
+export default LevelIcon;

6d78523a3e1f4bc7de1d45d1f38438d7cabd3132
iamsoojung <crystar2402@naver.com>
2024-10-02 03:02:22 +0900

    feat: 뉴스 CBF API 구현

--- recommend/app/main.py
+++ recommend/app/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.models import Base
+from app.database import SessionLocal, engine
+from app.recommendation import get_cbf_news
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/cbf-news/{user_id}")
+def read_recommendations(user_id: int, db: Session = Depends(get_db)):
+    recommendations = get_cbf_news(user_id, db)
+    if not recommendations:
+        raise HTTPException(status_code=404, detail="No recommendations found")
+    print(recommendations)
+    return {"recommendations": [{"title": news.title, "news_id": news.news_id} for news in recommendations]}
--- recommend/app/models.py
+++ recommend/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, BigInteger, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
-from database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = 'users'

65a0114851cbe088243e2d9bf796521060884b62
이예찬 <dldpcks34@naver.com>
2024-10-02 10:20:06 +0900

    refactor : 단어 추가 리팩토링 (#S11P21D105-373)
    전달할 때 html 수정

--- backend/src/main/java/com/newlearn/backend/proxy/ProxyController.java
+++ backend/src/main/java/com/newlearn/backend/proxy/ProxyController.java
@@ -1,5 +1,7 @@
 package com.newlearn.backend.proxy;
 
+import java.net.URLDecoder;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,14 @@ public class ProxyController {
 
 	@GetMapping("/api/proxy")
 	public ApiResponse<?> getWordData(@RequestParam String word) {
-
 		try {
+			System.out.println(word);
 			String url = "https://dic.daum.net/search.do?q=" + word;
 			String result = restTemplate.getForObject(url, String.class);
+			result = result.replace("\t", "");
+			result = result.replace("\n", "");
+			result = result.replace("\"", "");
+			System.out.println(result);
 			return ApiResponse.createSuccess(result, "성공적 조회");
 		} catch (Exception e) {
 			return ApiResponse.createError(ErrorCode.WORD_CREATE_FAILED);

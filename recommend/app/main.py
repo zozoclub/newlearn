c5f900dff104fdbc3498ab3ef3b018df24a2ce99
iamsoojung <crystar2402@naver.com>
2024-10-03 16:59:04 +0900

    fix: main.py 함수 수정

--- recommend/app/main.py
+++ recommend/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.models import Base
 from app.database import SessionLocal, engine
-from app.recommendation import get_cbf_news
-from app.hybrid_recommendation import get_cf_news, get_cbf_news
+# from app.recommendation import get_cbf_news
+from app.hybrid_recommendation import get_cf_news, get_cbf_news, hybrid_recommendation
 
 Base.metadata.create_all(bind=engine)
 
@@ -25,7 +25,7 @@ def read_cbf_recommendations(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="No CBF recommendations found")
     return {"recommendations": recommendations}
 
-# 협업 필터링 (CF) 뉴스 추천
+# 하이브리드 추천 시스템 - 협업 필터링 (CF)
 @app.get("/hybrid-recommendation/cf/{user_id}")
 def recommend_cf_news(user_id: int, db: Session = Depends(get_db)):
     recommended_news = get_cf_news(user_id, db)
@@ -33,10 +33,18 @@ def recommend_cf_news(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="No news recommendations available.")
     return recommended_news
 
-# 콘텐츠 기반 필터링 (CBF) 뉴스 추천
+# 하이브리드 추천 시스템 - 컨텐츠 기반 필터링 (CBF)
 @app.get("/hybrid-recommendation/cbf/{user_id}")
 def recommend_cf_news(user_id: int, db: Session = Depends(get_db)):
     recommended_news = get_cbf_news(user_id, db)
+    if not recommended_news:
+        raise HTTPException(status_code=404, detail="No news recommendations available.")
+    return recommended_news
+
+# 하이브리드 추천 시스템
+@app.get("/hybrid-recommendation/{user_id}")
+def recommend_hybrid_news(user_id: int, db: Session = Depends(get_db)):
+    recommended_news = hybrid_recommendation(user_id, db)
     if not recommended_news:
         raise HTTPException(status_code=404, detail="No news recommendations available.")
     return recommended_news

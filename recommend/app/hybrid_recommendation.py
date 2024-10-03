fe11eda3a66b2205d84784add64d04420f5dcd27
iamsoojung <crystar2402@naver.com>
2024-10-03 21:25:47 +0900

    fix: 코드 전반적인 수정

--- recommend/app/hybrid_recommendation.py
+++ recommend/app/hybrid_recommendation.py
@@ -14,29 +14,27 @@ from functools import lru_cache
 locale.setlocale(locale.LC_TIME, 'ko_KR.UTF-8')
 
 #####################################
-# 1. 사용자 데이터 가져 오기
+# 1. 사용자 데이터 가져오기
 #####################################
-# 해당 유저의 UserCategory (MySQL) 가져 오기
+# 해당 유저의 UserCategory (MySQL) 가져오기
 @lru_cache(maxsize=1024)
 def get_user_categories(user_id: int, db: Session):
     return db.query(UserCategory).filter(UserCategory.user_id == user_id).all()
 
-# User (MySQL) 가져 오기
+# User (MySQL) 가져오기
 @lru_cache(maxsize=1024)
 def get_user_difficulty(user_id: int, db: Session):
     user = db.query(User).filter(User.user_id == user_id).first()
-    if user:
-        return user.difficulty
-    return None
+    return user.difficulty if user else None
 
-# 해당 유저의 user_news_click (MongoDB) 가져 오기
+# 해당 유저의 user_news_click (MongoDB) 가져오기
 def get_user_click_log(user_id: int):
     return list(user_news_click.find({"user_id": user_id}))
 
 #####################################
 # 2. 데이터 전처리 및 유사도 계산
 #####################################
-def vectorize_titles(titles: List[str]) -> np.ndarray:
+def vectorize_titles(titles: List[str]) -> Tuple[np.ndarray, TfidfVectorizer]:
     vectorizer = TfidfVectorizer()
     return vectorizer.fit_transform(titles).toarray(), vectorizer
 
@@ -47,7 +45,10 @@ def find_similar_users(user_id: int, db: Session) -> List[int]:
     user_clicks = get_user_click_log(user_id)
     user_news_set = {click["news_id"] for click in user_clicks}
 
-    all_users_clicks = {user_click["user_id"]: get_user_click_log(user_click["user_id"]) for user_click in user_news_click.find({"user_id": {"$ne": user_id}})}
+    all_users_clicks = {
+        user_click["user_id"]: get_user_click_log(user_click["user_id"])
+        for user_click in user_news_click.find({"user_id": {"$ne": user_id}})
+    }
 
     all_news_ids = user_news_click.distinct("news_id")
     user_vector_map = {}
@@ -160,7 +161,6 @@ def get_user_time_pattern(user_id: int, db: Session) -> Dict[str, float]:
 #####################################
 # 추천 로직
 #####################################
-
 def collaborative_filtering(user_id: int, db: Session) -> List[tuple]:
     similar_users = find_similar_users(user_id, db)
     user_categories = get_user_categories(user_id, db)
@@ -196,77 +196,59 @@ def collaborative_filtering(user_id: int, db: Session) -> List[tuple]:
                         weight += 1  # 최신 뉴스에 가중치 1 추가
 
                 # 추천 뉴스 수집
-                recommended_news.append((click["news_id"], news_metadata.title, weight, news_metadata.published_date, news_metadata.hit))
+                recommended_news.append((click["news_id"], news_metadata.title, news_metadata.category_id, weight, news_metadata.published_date, news_metadata.hit))
 
     # 가중치 기준으로 추천 뉴스 정렬
     return sorted(recommended_news, key=lambda x: x[2], reverse=True)[:5]
 
-def content_based_filtering(user_id: int, db: Session):
+def content_based_filtering(user_id: int, db: Session) -> List[tuple]:
     user_categories = get_user_categories(user_id, db)
     user_category_ids = {uc.category_id for uc in user_categories}
-    user_clicks = get_user_click_log(user_id)
-    user_difficulty = get_user_difficulty(user_id, db)
-    user_time_pattern = get_user_time_pattern(user_id, db)
-
-    time_based_popularity = get_time_based_popularity(db)
-    difficulty_based_popularity = get_difficulty_based_popularity(db)
-
     recommended_news = []
     current_time = datetime.now()
 
     all_news = db.query(News).all()
-    clicked_titles = [news.title for news in all_news if news.news_id in [click["news_id"] for click in user_clicks]]
-    title_vectors, vectorizer = vectorize_titles(clicked_titles)
+    news_titles = [news.title for news in all_news]
 
-    for news in all_news:
-        if news.news_id not in [click["news_id"] for click in user_clicks]:
-            weight = 0
+    title_vectors, vectorizer = vectorize_titles(news_titles)
+    user_vector_index = -1  # 사용자 벡터 인덱스
 
-            # 1) 카테고리(관심도)에 따른 가중치
-            if news.category_id in user_category_ids:
-                weight += 2
-            else:
-                weight += 1
+    for index, news in enumerate(all_news):
+        if news.category_id in user_category_ids:
+            user_vector_index = index
+            break
 
-            # 2) 조회수에 따른 가중치
-            weight += news.hit / 100
+    if user_vector_index == -1:
+        print("사용자 카테고리에 맞는 뉴스가 없습니다.")
+        return []
 
-            # 3) 시간대별 인기도에 따른 가중치
-            news_time_popularity = time_based_popularity.get(news.news_id, {})
-            for time_period, popularity in news_time_popularity.items():
-                weight += popularity * user_time_pattern.get(time_period, 0) / 10
+    # 사용자 벡터와 모든 뉴스 벡터의 유사도 계산
+    user_vector = title_vectors[user_vector_index].reshape(1, -1)
+    similarity_scores = cosine_similarity(user_vector, title_vectors).flatten()
 
-            # 4) 난이도 스크랩 수에 따른 가중치
-            news_difficulty_popularity = difficulty_based_popularity.get(news.news_id, {})
-            weight += news_difficulty_popularity.get(user_difficulty, 0) / 100
-
-            # 5) 작성 시간(최신)에 따른 가중치
-            if isinstance(news.published_date, datetime):
-                time_diff = (current_time - news.published_date).total_seconds() / 3600
-                if time_diff < 24:
-                    weight += 1
-
-            # 추천 뉴스 수집
-            recommended_news.append((news.news_id, news.title, weight, news.published_date, news.hit))
+    for index, score in enumerate(similarity_scores):
+        if score > 0 and index != user_vector_index:
+            news_metadata = get_news_metadata(all_news[index].news_id, db)
+            if news_metadata:
+                weight = score  # 유사도 점수
+                recommended_news.append((news_metadata.news_id, news_metadata.title, news_metadata.category_id, weight, news_metadata.published_date, news_metadata.hit))
 
     # 가중치 기준으로 추천 뉴스 정렬
     return sorted(recommended_news, key=lambda x: x[2], reverse=True)[:5]
 
-def hybrid_recommendation(user_id: int, db: Session) -> List[Tuple[int, str, float, datetime, int]]:
+def hybrid_recommendation(user_id: int, db: Session) -> List[tuple]:
     collaborative_recommendations = collaborative_filtering(user_id, db)
     content_recommendations = content_based_filtering(user_id, db)
 
-    # 추천 뉴스 통합 및 가중치 조정
-    combined_recommendations = defaultdict(lambda: [0, None])  # [총 가중치, 뉴스]
+    all_recommendations = {news[0]: news for news in collaborative_recommendations + content_recommendations}
 
-    for news in collaborative_recommendations:
-        combined_recommendations[news[0]][0] += news[2]  # 가중치 추가
-        combined_recommendations[news[0]][1] = news
+    return sorted(all_recommendations.values(), key=lambda x: x[2], reverse=True)[:5]
 
-    for news in content_recommendations:
-        combined_recommendations[news[0]][0] += news[2]  # 가중치 추가
-        if combined_recommendations[news[0]][1] is None:
-            combined_recommendations[news[0]][1] = news
+def recommend_news(user_id: int, db: Session) -> List[tuple]:
+    user_difficulty = get_user_difficulty(user_id, db)
+    if user_difficulty is None:
+        print(f"사용자 ID {user_id}의 난이도 정보를 찾을 수 없습니다.")
+        return []
 
-    # 가중치 기준으로 추천 뉴스 정렬
-    return sorted(combined_recommendations.values(), key=lambda x: x[0], reverse=True)[:5]
+    recommendations = hybrid_recommendation(user_id, db)
+    return recommendations

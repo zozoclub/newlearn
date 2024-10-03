d3d7d8c964861d5b7fc16fef799108088212ca7d
노나현 <selene0106@naver.com>
2024-10-03 12:07:08 +0900

    fix: 최근 조회한 뉴스 응답DTO 한/영 다 주는 것으로 변경(#S11P21D105-600)

--- backend/src/main/java/com/newlearn/backend/news/dto/response/NewsSimpleResponseDTO.java
+++ backend/src/main/java/com/newlearn/backend/news/dto/response/NewsSimpleResponseDTO.java
@@ -8,18 +8,22 @@ import lombok.Getter;
 @Builder
 public class NewsSimpleResponseDTO {
     private Long newsId;
-    private String title;
-    private String content;
+    private String titleEn;
+    private String titleKr;
+    private String contentEn;
+    private String contentKr;
     private String thumbnailImageUrl;
     private String category; //"정치", "경제", "사회", "생활/문화", "IT/과학", "세계"
     private String publishedDate;
 
 
-    public static NewsSimpleResponseDTO makeNewsSimpleResponseDTO(News news, String lang, int difficulty) {
+    public static NewsSimpleResponseDTO makeNewsSimpleResponseDTO(News news) {
         return NewsSimpleResponseDTO.builder()
                 .newsId(news.getNewsId())
-                .title(news.getTitleByLang(lang))
-                .content(news.getContentByLangAndDifficulty(lang, difficulty))
+                .titleEn(news.getTitleEng())
+                .titleKr(news.getTitle())
+                .contentEn(news.getContentTranslationMedium())
+                .contentKr(news.getContentKoreanMedium())
                 .thumbnailImageUrl(news.getThumbnailImageUrl())
                 .category(news.getCategory().getCategoryName())
                 .publishedDate(news.getPublishedDate())

3e86ad581510a3969d09d130e057999541b694c9
손민락 <dungguk@naver.com>
2024-10-07 10:15:16 +0900

    fix: 최근 읽은 뉴스를 상세 페이지에 추가함에 따라 컨텐츠 필터링 추천 개수를 5개로 줄임

--- frontend/src/components/newspage/FilteredRecommendNews.tsx
+++ frontend/src/components/newspage/FilteredRecommendNews.tsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { NewsType } from "types/newsType";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import styled, { useTheme } from "styled-components";
+import { useTheme } from "styled-components";
 import Spinner from "@components/Spinner";
 import LoadingDiv from "./LoadingDiv";
 import LoadingBar from "./LoadingBar";
@@ -17,7 +17,7 @@ const FilteredRecommendNews = () => {
   const languageData = useRecoilValue(languageState);
   const { newsId } = useParams();
   const { isLoading, data } = useQuery<NewsType[]>({
-    queryKey: ["getFilteredRecommendNews"],
+    queryKey: ["getFilteredRecommendNews", Number(newsId)],
     queryFn: () => getContentsFilteringRecommendNewsList(Number(newsId)),
     staleTime: 5 * 60 * 1000,
     enabled: newsId !== null,
@@ -26,11 +26,11 @@ const FilteredRecommendNews = () => {
   const transitionTo = usePageTransition();
   const MemoizedComponent = useMemo(() => {
     return (
-      <Container>
+      <>
         <div className="header">이 뉴스와 비슷한 뉴스</div>
         {isLoading ? (
           <div className="recommand-news">
-            {[...Array(6)].map((_, index) => (
+            {[...Array(5)].map((_, index) => (
               <div style={{ display: "flex" }} key={index}>
                 <div
                   className="content-div"
@@ -59,7 +59,7 @@ const FilteredRecommendNews = () => {
           </div>
         ) : (
           <div className="recommand-news">
-            {data?.map((news) => (
+            {data?.slice(0, Math.min(data?.length, 5)).map((news) => (
               <div
                 className="news-div"
                 key={news.newsId}
@@ -99,80 +99,12 @@ const FilteredRecommendNews = () => {
             ))}
           </div>
         )}
-      </Container>
+      </>
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   return MemoizedComponent;
 };
 
 export default FilteredRecommendNews;
-
-const Container = styled.div`
-  border-radius: 0.75rem;
-  width: 30%;
-  padding: 0 0 2% 0;
-  .header {
-    font-size: 1.25rem;
-    font-weight: 600;
-    margin-bottom: 1rem;
-    padding: 0.5rem;
-  }
-  .recommand-news {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-  }
-  .news-div {
-    display: flex;
-    border-radius: 0.5rem;
-    padding: 0.5rem;
-    background-color: ${(props) =>
-      props.theme.mode === "dark" ? "transparent" : "#ffffffaa"};
-    backdrop-filter: blur(10px);
-    border: 2px solid
-      ${(props) => (props.theme.mode === "dark" ? "#1a1a1aaa" : "#e2e2e2aa")};
-    cursor: pointer;
-    transform: translateY(0rem);
-    transition: background-color 0.3s, transform 0.3s, border 0.3s,
-      backdrop-filter 0.3s;
-    &:hover {
-      background-color: ${(props) =>
-        props.theme.mode === "dark"
-          ? props.theme.colors.cardBackground01
-          : "transparent"};
-      transform: translateY(-0.25rem);
-    }
-  }
-  .content-div {
-    width: 52.5%;
-    padding: 0 2.5% 0 0;
-    font-size: 1.125rem;
-    line-height: 1.25rem;
-    height: 3.75rem;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-  }
-  .thumbnail-div {
-    position: relative;
-    width: 45%;
-    aspect-ratio: 1.6;
-    img {
-      width: 100%;
-      aspect-ratio: 1.6;
-      object-fit: cover;
-      border-radius: 0.5rem;
-    }
-  }
-  @media (max-width: 1279px) {
-    width: 98%;
-    .content-div {
-      width: 67.5%;
-    }
-    .thumbnail-div {
-      width: 30%;
-    }
-  }
-`;

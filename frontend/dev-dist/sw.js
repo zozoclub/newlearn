9d0d28b463f4b74521c797416c36879373a6b9b8
허세령 <seryoii@naver.com>
2024-10-07 16:29:30 +0900

    fix: 마이페이지 PWA css 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cejv4283q98"
+    "revision": "0.dsi53ou97ko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

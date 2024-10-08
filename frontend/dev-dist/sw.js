cbfa024bb7a1554a4fb2614e47eedc301c94502b
손민락 <dungguk@naver.com>
2024-10-08 09:14:00 +0900

    fix: 랭킹 리스트 hooks 로직 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d4qbvig2qt8"
+    "revision": "0.4grhg9utvag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

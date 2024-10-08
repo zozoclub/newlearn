55998f7419292f52b71edd83b14db6b7957d1629
손민락 <dungguk@naver.com>
2024-10-08 09:47:58 +0900

    fix: language를 바꿔도 추천 뉴스의 언어가 바뀌지 않는 문제 수정

--- frontend/src/components/newspage/FilteredRecommendNews.tsx
+++ frontend/src/components/newspage/FilteredRecommendNews.tsx
@@ -102,7 +102,7 @@ const FilteredRecommendNews = () => {
       </>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
+  }, [data, languageData]);
 
   return MemoizedComponent;
 };
--- frontend/src/components/newspage/RecentReadNews.tsx
+++ frontend/src/components/newspage/RecentReadNews.tsx
@@ -102,7 +102,7 @@ const RecentReadNews = () => {
       </>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
+  }, [data, languageData]);
 
   return MemoizedComponent;
 };

5010bff451c076949daa1edbea048bac03867e35
박효진 <jam_ty@naver.com>
2024-10-08 09:19:43 +0900

    remove : 불필요 컴포넌트 제거

--- frontend/src/pages/mobile/SpeakingTestMobilePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SpeakingTestMobilePage:React.FC = () => {
-  return <div>SpeakingTestMobilePage</div>;
-};
-
-export default SpeakingTestMobilePage;
--- frontend/src/pages/mobile/SpeakingTestResultListMobilePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SpeakingTestResultListMobilePage:React.FC = () => {
-  return <div>SpeakingTestResultListMobilePage</div>;
-};
-
-export default SpeakingTestResultListMobilePage;

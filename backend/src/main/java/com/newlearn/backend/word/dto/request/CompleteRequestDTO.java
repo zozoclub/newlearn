78745d77173d2d9db981556c8beb2cc521161608
이예찬 <dldpcks34@naver.com>
2024-10-09 15:53:34 +0900

    refactor: 단어 삭제나, 완료 시 id 하나가 아닌 여러개 제공하도록 리팩토링

---
+++ backend/src/main/java/com/newlearn/backend/word/dto/request/CompleteRequestDTO.java
@@ -0,0 +1,13 @@
+package com.newlearn.backend.word.dto.request;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CompleteRequestDTO {
+
+	private List<Long> wordIds;
+}

ffd2960c6ea54382514c5b32bc3689e52e7d5655
손민락 <dungguk@naver.com>
2024-10-10 11:20:55 +0900

    design: PWA 회원가입 단어 잘리는 문제 수정

--- frontend/src/components/signuppage/LevelTest.tsx
+++ frontend/src/components/signuppage/LevelTest.tsx
@@ -279,14 +279,15 @@ const WordTest = styled.div`
 `;
 const WordGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  place-items: center;
+  grid-template-columns: repeat(2, minmax(0, 2fr));
   grid-template-rows: repeat(8, 1fr);
   gap: 1rem 2rem;
-  max-width: 40rem; // 최대 너비 설정 (필요에 따라 조정)
+  width: 100%;
 `;
 
 const WordButton = styled.button<{ $isSelected: boolean }>`
-  width: 185px;
+  width: 100%;
   font-weight: bold;
   padding: 0.75rem 1rem;
   font-size: 1.125rem;

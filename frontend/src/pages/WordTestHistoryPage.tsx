8e183966695e2a2031eba8905bfd3b511bbb8f3b
박효진 <jam_ty@naver.com>
2024-10-10 12:54:02 +0900

    feat : backheader 추가

--- frontend/src/pages/WordTestHistoryPage.tsx
+++ frontend/src/pages/WordTestHistoryPage.tsx
@@ -4,80 +4,84 @@ import locationState from "@store/locationState";
 import styled from "styled-components";
 import { useQuery } from "@tanstack/react-query";
 import {
-    MemorizeWordListResponseDto,
-    getMemorizeWordList,
+  MemorizeWordListResponseDto,
+  getMemorizeWordList,
 } from "@services/wordMemorize";
 import StartWordTestWidget from "@components/testpage/StartWordTestWidget";
 import WordTestHistory from "@components/testpage/WordTestHistory";
 import { useMediaQuery } from "react-responsive";
 import HeaderMobile from "@components/common/HeaderMobile";
+import BackArrow from "@assets/icons/BackArrow";
 
 type Word = {
-    id: string;
-    title: string;
-    content: string;
+  id: string;
+  title: string;
+  content: string;
 };
 
 const WordTestHistoryPage: React.FC = () => {
-    const setCurrentLocationData = useSetRecoilState(locationState);
-    useEffect(() => {
-      setCurrentLocationData("wordHistory");
-      return () => {
-        setCurrentLocationData("");
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  const setCurrentLocationData = useSetRecoilState(locationState);
+  useEffect(() => {
+    setCurrentLocationData("wordHistory");
+    return () => {
+      setCurrentLocationData("");
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
-    const { data: wordList } = useQuery<MemorizeWordListResponseDto[]>({
-        queryKey: ["memorizeWordList"],
-        queryFn: () => getMemorizeWordList(),
-    });
+  const { data: wordList } = useQuery<MemorizeWordListResponseDto[]>({
+    queryKey: ["memorizeWordList"],
+    queryFn: () => getMemorizeWordList(),
+  });
 
-    const [toStudyWords, setToStudyWords] = useState<Word[]>([]);
+  const [toStudyWords, setToStudyWords] = useState<Word[]>([]);
 
-    useEffect(() => {
-        if (wordList) {
-            const toStudy = wordList
-                .filter((word) => !word.complete)
-                .map((word) => ({
-                    id: word.wordId.toString(),
-                    title: word.word,
-                    content: word.wordMeaning,
-                }));
-            setToStudyWords(toStudy);
-        }
-    }, [wordList]);
-
-
-    if (isMobile) {
-        return (
-            <>
-                <HeaderMobile title="Word Test" url="/mystudy"/>
-                <MobileLayout>
-                    <StartWordTestWidget posibleWords={toStudyWords.length} />
-                    <WordTestHistory />
-                </MobileLayout>
-            </>
-        )
+  useEffect(() => {
+    if (wordList) {
+      const toStudy = wordList
+        .filter((word) => !word.complete)
+        .map((word) => ({
+          id: word.wordId.toString(),
+          title: word.word,
+          content: word.wordMeaning,
+        }));
+      setToStudyWords(toStudy);
     }
+  }, [wordList]);
 
+  if (isMobile) {
     return (
-        <Layout>
-            <LeftContainer>
-                <SmallContainer>
-                    <StartWordTestWidget posibleWords={toStudyWords.length} />
-                </SmallContainer>
-            </LeftContainer>
-
-            <RightContainer>
-                <BigContainer>
-                    <WordTestHistory />
-                </BigContainer>
-            </RightContainer>
-        </Layout>
+      <>
+        <HeaderMobile title="Word Test" url="/mystudy" />
+        <MobileLayout>
+          <StartWordTestWidget posibleWords={toStudyWords.length} />
+          <WordTestHistory />
+        </MobileLayout>
+      </>
     );
+  }
+
+  return (
+    <Layout>
+      <LeftContainer>
+        <SmallContainer>
+          <BackHeader>
+            <BackArrow width={48} height={48} url="/mystudy" />
+            <BackHeaderText>스터디로 돌아가기</BackHeaderText>
+          </BackHeader>
+          <StartWordTestWidget posibleWords={toStudyWords.length} />
+        </SmallContainer>
+      </LeftContainer>
+
+      <RightContainer>
+        <BigContainer>
+          <WordTestHistory />
+        </BigContainer>
+      </RightContainer>
+    </Layout>
+  );
 };
 
 export default WordTestHistoryPage;
@@ -106,8 +110,7 @@ const RightContainer = styled.div`
 
 const SmallContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   height: 46rem;
   background-color: ${(props) => props.theme.colors.cardBackground01};
   backdrop-filter: blur(0.25rem);
@@ -132,7 +135,22 @@ const BigContainer = styled.div`
 `;
 
 const MobileLayout = styled.div`
-width: 85%;
+  width: 85%;
   margin: 0 auto;
   padding-bottom: 6rem;
+`;
+
+const BackHeader = styled.div`
+  display: flex;
+  margin-right: auto;
+  align-items: center;
+  padding-top : 0.5rem;
+  padding-left: 1rem;
+  font-size: 1.5rem;
+  font-weight: 700;
+`;
+
+
+const BackHeaderText = styled.span`
+margin-left: 1rem;
 `

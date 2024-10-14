4d8b22a75f467e44f2948cbce8c02f3aa0816b50
손민락 <dungguk@naver.com>
2024-10-14 17:28:05 +0900

    fix: 뉴스 목록 페이지 뉴스 추천도 함께 스크롤 될 수 있게 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v4o8diu98eg"
+    "revision": "0.gfmljqligo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
